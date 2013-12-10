4ab4dd1ffdca96fe320d1e1ee3363aa053a9ddf5
Jasper St. Pierre <jstpierre@mecheye.net>
2013-12-10 10:56:11 -0500

    Make compressed SRT

--- func.js
+++ func.js
@@ -29,41 +29,6 @@
         return labels;
     }
 
-    function sortMessages(labels) {
-        var times = {};
-        times[0] = [];
-        labels.forEach(function(label) {
-            if (!times[label.start])
-                times[label.start] = [];
-            times[label.start].push({ op: 'add', message: label.message });
-
-            if (!times[label.stop])
-                times[label.stop] = [];
-            times[label.stop].push({ op: 'remove', message: label.message });
-        });
-
-        var activeMessages = {};
-        var timeline = [];
-        var sortedTimes = Object.keys(times).map(function(S) {
-            return parseInt(S, 10);
-        }).sort(function(a, b) {
-            return a - b;
-        });
-        sortedTimes.forEach(function(time) {
-            var ops = times[time];
-            ops.forEach(function(op) {
-                if (op.op === 'add')
-                    activeMessages[op.message] = 1;
-                if (op.op === 'remove')
-                    delete activeMessages[op.message];
-            });
-
-            timeline.push({ time: time, messages: Object.keys(activeMessages) });
-        });
-
-        return timeline;
-    }
-
     function srtTimecode(msecs) {
         var ms = Math.floor(msecs % 1000);
         var value = msecs / 1000;
@@ -84,40 +49,20 @@
         return (pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ',' + pad(ms, 3));
     }
 
-    function formatChunk(chunkNum, start, stop, messages) {
+    function formatSrtChunk(label, i) {
         var lines = [];
 
         lines.push('');
-        lines.push(chunkNum);
-        lines.push(srtTimecode(start) + '-->' + srtTimecode(stop));
-        lines.push.apply(lines, messages);
+        lines.push(i + 1);
+        lines.push(srtTimecode(label.start) + '-->' + srtTimecode(label.stop));
+        lines.push(label.message);
 
         return lines.join('\n');
     }
 
-    function makeSrt(timeline) {
-        var chunks = [];
-        var chunkNum = 0;
-        for (var i = 0; i < timeline.length; i++) {
-            var messages = timeline[i].messages;
-            if (!messages.length)
-                continue;
-
-            var start = timeline[i].time;
-            var stop = timeline[i+1].time;
-
-            chunks.push(formatChunk(chunkNum++, start, stop, messages));
-        }
-
-        return chunks.join('\n');
-    }
-
     function labelsToSrt(labels) {
         var lines = labels.split('\n');
-
-        var messages = parseLabels(lines);
-        var timeline = sortMessages(messages);
-        var srt = makeSrt(timeline);
+        var srt = parseLabels(lines).map(formatSrtChunk).join('\n');
         return srt;
     }
 
